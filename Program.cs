ba1e6fde0248a52acdfac1b89a353c8504d0bad9
Melissa Gwyn <melissagwyn@gmail.com>
2023-02-13 14:59:55 -0500

    Queues

--- Program.cs
+++ Program.cs
@@ -1,69 +1,90 @@
 ﻿using System;
-using System.Collections.Generic; 
+using System.Collections.Generic;
 
 namespace Collections
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Program
     {
-      var names = new List<string>() { "Mark", "Paul", "Sandy", "Bill" };
+        static void Main(string[] args)
+        {
+            var names = new List<string>() { "Mark", "Paul", "Sandy", "Bill" };
 
-      var firstName = names[0];
-      var secondName = names[1];
-      var finalName = names[3];
+            var firstName = names[0];
+            var secondName = names[1];
+            var finalName = names[3];
 
-      Console.WriteLine (firstName);
-      Console.WriteLine (names.Count);  
+            Console.WriteLine(firstName);
+            Console.WriteLine(names.Count);
 
-      names.Add("George");
-      Console.WriteLine(names.Count); 
+            names.Add("George");
+            Console.WriteLine(names.Count);
 
-      names.Insert(2, "Frank");
+            names.Insert(2, "Frank");
 
-      var scores = new List<int>();
-      scores.Add (12);
-      scores.Add (100);
-      scores.Add (55);
-      scores.Add (44);
-      scores.Add (12);
+            var scores = new List<int>();
+            scores.Add(12);
+            scores.Add(100);
+            scores.Add(55);
+            scores.Add(44);
+            scores.Add(12);
 
-      var lastIndex = scores.Count -1;
-      var lastScore = scores[lastIndex];
+            var lastIndex = scores.Count - 1;
+            var lastScore = scores[lastIndex];
 
-      var indexOfFiftyFive = scores.IndexOf(55);
-      Console.WriteLine ($"Found 55 at index {indexOfFiftyFive}");
+            var indexOfFiftyFive = scores.IndexOf(55);
+            Console.WriteLine($"Found 55 at index {indexOfFiftyFive}");
 
-      var indexOfFortyTwo = scores.IndexOf(42);
-      Console.WriteLine($"Found 42 at index {indexOfFortyTwo}");
+            var indexOfFortyTwo = scores.IndexOf(42);
+            Console.WriteLine($"Found 42 at index {indexOfFortyTwo}");
 
-      var indexOfTwelve = scores.LastIndexOf(12);
-      Console.WriteLine($"Found 12 at index {indexOfTwelve}");
+            var indexOfTwelve = scores.LastIndexOf(12);
+            Console.WriteLine($"Found 12 at index {indexOfTwelve}");
 
-      var playerScores = new Dictionary<string, int>();
+            var playerScores = new Dictionary<string, int>();
 
-      playerScores.Add("Robbie Lakeman", 1_234_567);
-      playerScores["Robbie Lakeman"] = 3;
-      playerScores["Gavin Stark"] = 42;
-
-
-      var robbiePlayerScore = playerScores["Robbie Lakeman"];
-      Console.WriteLine(robbiePlayerScore);
-
-      var gavinScore = playerScores["Gavin Stark"];
-      Console.WriteLine($"Gavin's score is {gavinScore}");
-
-      var hasKey = playerScores.ContainsKey("Billy Mitchell");
-      if (hasKey == true)
-      {
-        var billyScore = playerScores["Billy Mitchell"];
-        Console.WriteLine(billyScore);
-      }
+            playerScores.Add("Robbie Lakeman", 1_234_567);
+            playerScores["Robbie Lakeman"] = 3;
+            playerScores["Gavin Stark"] = 42;
 
 
+            var robbiePlayerScore = playerScores["Robbie Lakeman"];
+            Console.WriteLine(robbiePlayerScore);
+
+            var gavinScore = playerScores["Gavin Stark"];
+            Console.WriteLine($"Gavin's score is {gavinScore}");
+
+            var hasKey = playerScores.ContainsKey("Billy Mitchell");
+            if (hasKey == true)
+            {
+                var billyScore = playerScores["Billy Mitchell"];
+                Console.WriteLine(billyScore);
+            }
+
+            var students = new Queue<string>();
+            students.Enqueue("Mary");
+            students.Enqueue("Bill");
+            students.Enqueue("Paul");
+            students.Enqueue("Sandra");
+            students.Enqueue("Thomas");
+
+            var firstStudent = students.Dequeue();
+            Console.WriteLine(firstStudent);
+
+            // foreach (var student in students)
+            // {
+            //     Console.WriteLine($"Hello {student}");
+            // }
+
+            //var student = students.Dequeue();
+            //Console.WriteLine($"Goodbye {student}");
+            // Console.WriteLine($"There are now {students.Count} students in the queue.");
+
+            // var student = students.Peek;
+            // Console.WriteLine($"You are up next {student}");
 
 
 
-    } 
-  }
+
+        }
+    }
 }
